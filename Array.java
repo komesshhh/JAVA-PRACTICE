36c9e7bfcfb3b053ce9af1a5c3728ebc232f1091
komesshhh <komeshsiva@gmail.com>
2022-08-07 08:40:17 +0530

    Print the nexr char in alphabets as string

--- Array.java
+++ Array.java
@@ -1,12 +1,23 @@
+
 package com.komesh.Intrduction;
-
-public class Array {
-
-	public static void main(String[] args)
-	{	
-	
-	
-	    }
-		
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Array
+{
+	public static void main(String []args) {
+		int c =' ';
+		char d=' ';
+		String e="";
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		char []b=a.toCharArray();
+		for(int i=0;i<a.length();i++) {
+			c=(int) b[i]+1;
+			d= (char) c;
+			e=""+d;
+			System.out.print(e.replace("!"," "));
+		}
 	}
 
+}
